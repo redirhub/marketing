f05c825fbf718e27e04f3e0f0cf4f8b36c9fc169
Krisbo <24961323+krisboblea@users.noreply.github.com>
2026-01-14 13:07:42 +0200

    add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from 'react'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        {children}
+        {gaId && <GoogleAnalytics gaId={gaId} />}
+      </body>
     </html>
   )
 }
