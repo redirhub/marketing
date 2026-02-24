ff0e088d55448ecb38505a2af406a153c39ac9f7
Krisbo <24961323+krisboblea@users.noreply.github.com>
2026-02-25 01:23:00 +0400

    fix: I made basePath required in BlogList since both blog pages now always provide it

--- src/components/blogs/BlogList.tsx
+++ src/components/blogs/BlogList.tsx
@@ -8,7 +8,7 @@ import type { PostPreview } from "@/types/sanity";
 interface BlogListProps {
   currentPage: number;
   locale?: string;
-  basePath?: string;
+  basePath: string;
 }
 
 const BlogList = async ({ currentPage, locale = "en", basePath }: BlogListProps) => {
